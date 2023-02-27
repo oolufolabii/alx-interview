ae1931ee0810d89d41b6a06f2e98e5bf3f507df4
oolufolabii <otitoola.olufolabi@gmail.com>
2023-02-27 19:43:45 +0100

    N Queens

--- 0x05-nqueens/0-nqueens.py
+++ 0x05-nqueens/0-nqueens.py
@@ -2,3 +2,28 @@
 
 """Python module on N queens puzzle"""
 
+import sys
+
+possible_solutions = []
+# n = 0
+possible_position = None
+
+
+def user_input():
+    """Get and validate the users input or argument"""
+    global j
+    j = 0
+    if len(sys.argv) != 2:
+        print("Usage: nqueens N")
+        sys.exit(1)
+
+    try:
+        j = int(sys.argv[1])
+    except Exception:
+        print("N must be a number")
+        sys.exit(1)
+    
+    if j < 4:
+        print("N must be at least 4")
+        sys.exit(1)
+    return j
