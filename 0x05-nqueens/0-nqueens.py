26d9564b460427071d4ef9103be828d01712bdba
oolufolabii <otitoola.olufolabi@gmail.com>
2023-02-27 20:09:53 +0100

    N Queens

--- 0x05-nqueens/0-nqueens.py
+++ 0x05-nqueens/0-nqueens.py
@@ -22,11 +22,18 @@ def user_input():
     except Exception:
         print("N must be a number")
         sys.exit(1)
-    
+
     if j < 4:
         print("N must be at least 4")
         sys.exit(1)
     return j
 
+def attacking_queen(position_1, position_2):
+	"""Checker for attacking position of the Queen.
+	Returns True if the queens are in an attacking
+	position else False."""
+	if ([position_1][0] == position_2[0]) or ([position_1][1] == position_2[1]):
+		return True
+	return abs([position_1][0] - position_2[0]) == abs([position_1][1] - position_2[1])
 
 # print(user_input())
