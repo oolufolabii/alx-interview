94d3aa00c7ff6be96ba4de30b42ad89ccc70b833
oolufolabii <otitoola.olufolabi@gmail.com>
2023-03-14 11:05:54 +0100

    SE Interviews

--- 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -6,31 +6,52 @@
 def rotate_2d_matrix(matrix):
     """Given an n x n 2D matrix, rotate it 90 degrees clockwise."""
     # list(zip(*matrix[::-1]))
+    # if type(matrix) != list:
+    #     return
+
+    # if len(matrix) <= 0:
+    #     return
+
+    # if not all(map(lambda x: type(x) == list, matrix)):
+    #     return
+
+    # matrix_rows = len(matrix)
+    # matrix_cols = len(matrix[0])
+
+    # if not all(map(lambda x: len(x) == matrix_cols, matrix)):
+    #     return
+
+    # col, row = 0
+    # matrix_rows - 1
+
+    # for j in range(matrix_cols * matrix_rows):
+    #     if j % matrix_rows == 0:
+    #         matrix.append([])
+    #     if row == -1:
+    #         row = matrix_rows - 1
+    #         col += 1
+    #     matrix[-1].append(matrix[row][col])
+    #     if col == matrix_cols - 1 and row >= -1:
+    #         matrix.pop(row)
+    #     row -= 1
     if type(matrix) != list:
         return
-
     if len(matrix) <= 0:
         return
-
     if not all(map(lambda x: type(x) == list, matrix)):
         return
-
-    matrix_rows = len(matrix)
-    matrix_cols = len(matrix[0])
-
-    if not all(map(lambda x: len(x) == matrix_cols, matrix)):
+    rows = len(matrix)
+    cols = len(matrix[0])
+    if not all(map(lambda x: len(x) == cols, matrix)):
         return
-
-    col, row = 0
-    matrix_rows - 1
-
-    for j in range(matrix_cols * matrix_rows):
-        if j % matrix_rows == 0:
+    c, r = 0, rows - 1
+    for i in range(cols * rows):
+        if i % rows == 0:
             matrix.append([])
-        if row == -1:
-            row = matrix_rows - 1
-            col += 1
-        matrix[-1].append(matrix[row][col])
-        if col == matrix_cols - 1 and row >= -1:
-            matrix.pop(row)
-        row -= 1
+        if r == -1:
+            r = rows - 1
+            c += 1
+        matrix[-1].append(matrix[r][c])
+        if c == cols - 1 and r >= -1:
+            matrix.pop(r)
+        r -= 1
