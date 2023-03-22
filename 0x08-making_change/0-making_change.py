ae5d4a8cced5b1586e7ce85bebb8e966656172fd
oolufolabii <otitoola.olufolabi@gmail.com>
2023-03-22 09:57:18 +0100

    SE Interviews

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -1,3 +1,27 @@
 #!/usr/bin/python3
 
 """Python module for returning a change"""
+
+
+def makeChange(coins, total):
+    """
+    Given a pile of coins of different values, determine the
+    fewest number of coins needed to meet a given amount total.
+    """
+
+    if total <= 0:
+        return 0
+    coins_left = total
+    count = 0
+    index = 0
+    sorted_coins = sorted(coins, reverse=True)
+    n = len(coins)
+    while coins_left > 0:
+        if index >= n:
+            return -1
+        if coins_left - sorted_coins[index] >= 0:
+            coins_left -= sorted_coins[index]
+            count += 1
+        else:
+            index += 1
+    return count
