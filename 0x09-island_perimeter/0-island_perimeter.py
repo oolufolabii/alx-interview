ccc6b6fb462dddb850599fa99fd78dc217fd12b7
oolufolabii <otitoola.olufolabi@gmail.com>
2023-03-27 14:20:31 +0100

    Island Interview

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -1,6 +1,31 @@
 #!/usr/bin/python3
 """Python Module for Grid Perimeter"""
+from typing import List
 
 
-def island_perimeter(grid):
+def island_perimeter(grid: List[List[int]]) -> int:
     """returns the perimeter of the island described in grid"""
+
+    visited = set()
+
+    def depth_search(i, j):
+        if i >= len(grid) or j >= len(grid[0]) or \
+                i < 0 or j < 0 or grid[i][j] == 0:
+            return 1
+
+        if (i, j) in visited:
+            return 0
+
+        visited.add((i, j))
+
+        perimeter = depth_search(i, j+1)
+        perimeter += depth_search(i+1, j)
+        perimeter += depth_search(i, j-1)
+        perimeter += depth_search(i-1, j)
+
+        return perimeter
+
+    for i in range(len(grid)):
+        for j in range(len(grid[0])):
+            if grid[i][j]:
+                return depth_search(i, j)
