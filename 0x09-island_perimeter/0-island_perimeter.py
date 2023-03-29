70366cd96a107a1a9341054e0b4c77e5db415145
oolufolabii <otitoola.olufolabi@gmail.com>
2023-03-29 20:01:52 +0100

    Island Interview

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -1,23 +1,19 @@
 #!/usr/bin/python3
-"""Python Module for Grid Perimeter"""
-from typing import List
+"""returns the perimeter of the island described in grid"""
 
 
-def island_perimeter(grid: List[List[int]]) -> int:
+def island_perimeter(grid):
     """returns the perimeter of the island described in grid"""
+    perimeter = 0
+    for i in range(len(grid)):
+        for j in range(len(grid[i])):
+            if grid[i][j] == 1:
+                perimeter += 4
+                # cells with 2 sides touching other cells on top and bottom
+                if i > 0 and grid[i - 1][j] == 1:
+                    perimeter -= 2
+                # cells with 2 sides touching other cells left and right
+                if j > 0 and grid[i][j - 1] == 1:
+                    perimeter -= 2
 
-    def search(matrix):
-        """Counts the perimeter using depth search"""
-        count = 0
-        for row in matrix:
-            row = [0] + row + [0]
-            for i in range(1, len(row)):
-                count += row[i] != row[i-1]
-        return count
-
-    tgrid = [[] for _ in range(len(grid[0]))]
-    for row in grid:
-        for i, item in enumerate(row):
-            tgrid[i].append(item)
-
-    return search(grid) + search(tgrid)
+    return perimeter
