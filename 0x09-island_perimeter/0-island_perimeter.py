37146c46224db4b12ce419fc93bf3d6590e6a5c4
oolufolabii <otitoola.olufolabi@gmail.com>
2023-03-29 20:05:05 +0100

    Island Interview

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python3
-"""returns the perimeter of the island described in grid"""
+"""Python Module for Grid Perimeter"""
+from typing import List
 
 
-def island_perimeter(grid):
-    """returns the perimeter of the island described in grid"""
+def island_perimeter(grid: List[List[int]]) -> int:
+    """Returns the perimeter of the island described in grid"""
+
     perimeter = 0
     for i in range(len(grid)):
         for j in range(len(grid[i])):
